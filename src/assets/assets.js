0761d438b3c16b963dd399ac17829c7a9cf34e1b
Ayush Gawali <ayushgawali.12345@gmail.com>
2025-03-01 19:59:18 +0530

    Final Design v-1.0

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -119,5 +119,5 @@ export const infoList = [
 ];
 
 export const toolsData = [
-    assets.vscode, assets.mongodb, assets.figma, assets.git
+    assets.vscode, assets.firebase ,assets.mongodb, assets.figma, assets.git
 ];
